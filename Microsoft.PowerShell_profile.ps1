8a1836dbabf85924226c1b748e1b51b75cfd8305
Jason Morgan <jmorgan@f9vs.com>
2014-04-13 12:42:04 -0600

    Updated Prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,10 @@
-cd c:\isdps
-start-job -ScriptBlock {Update-Help -Force}
+New-PSDrive -Name VMStore -PSProvider FileSystem -Root '\\VMWare-host\Shared Folders' | out-null
+start-job -ScriptBlock {Update-Help -Force} | Out-Null
+cd c:\git
 function ql { $args }
-function qs ([string]$Separator) { $OFS = $Separator ; "$args" }
+function qs ([string]$Separator) { if ($Separator) {$OFS = $Separator} ; "$args" }
 function mf {Param ($path) new-item -itemtype file -path $path -force}
-$host.ui.RawUI.WindowTitle = "Jason`'s Rocking PowerShell Console"
+$host.ui.RawUI.WindowTitle = "Jason`'s Rocking PowerShell Console: $([Math]::Round((gps -id $PID).vm/1mb,2)) MB Memory used"
 function prompt { 
    # our theme
    $cdelim = [ConsoleColor]::DarkCyan 
@@ -14,9 +15,8 @@ function prompt {
    Write-Host "[$(get-location)]" -NoNewline
    Write-Host " - Yes Master?" -ForegroundColor Cyan -NoNewline
    Write-Host " > " -NoNewline
+   $host.ui.RawUI.WindowTitle = "Jason`'s Rocking PowerShell Console: $([Math]::Round((gps -id $PID).vm/1mb,2)) MB Memory used"
    return ' '
 } 
-$git = 'C:\Users\z598631\Documents\GitHub'
-$kit = 'C:\Users\z598631\Documents\GitHub\Kitton_Mittons'
-$docs = 'C:\Users\z598631\Documents'
-$Down = 'C:\Users\z598631\Downloads'
+$docs = 'VMDrive:\Documents'
+$Down = 'VMDrive:\Downloads'
