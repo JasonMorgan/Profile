ec318f38aad4613ef32fc165d3bed2ae918a5e1e
Jason Morgan <jmorgan@f9vs.com>
2014-04-19 09:33:04 -0600

    Added edit
    Also removed new-psdrive with #

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,10 @@
-New-PSDrive -Name VMStore -PSProvider FileSystem -Root '\\VMWare-host\Shared Folders' | out-null
+#New-PSDrive -Name VMStore -PSProvider FileSystem -Root '\\VMWare-host\Shared Folders' | out-null
 start-job -ScriptBlock {Update-Help -Force} | Out-Null
 cd c:\git
 function ql { $args }
 function qs ([string]$Separator) { if ($Separator) {$OFS = $Separator} ; "$args" }
 function mf {Param ($path) new-item -itemtype file -path $path -force}
-$host.ui.RawUI.WindowTitle = "Jason`'s Rocking PowerShell Console: $([Math]::Round((gps -id $PID).vm/1mb,2)) MB Memory used"
+$host.ui.RawUI.WindowTitle = "Jason`'s Rocking PowerShell Console: $([Math]::Round((gps -id $PID).WorkingSet/1mb,2)) MB Memory used"
 function prompt { 
    # our theme
    $cdelim = [ConsoleColor]::DarkCyan 
@@ -15,8 +15,9 @@ function prompt {
    Write-Host "[$(get-location)]" -NoNewline
    Write-Host " - Yes Master?" -ForegroundColor Cyan -NoNewline
    Write-Host " > " -NoNewline
-   $host.ui.RawUI.WindowTitle = "Jason`'s Rocking PowerShell Console: $([Math]::Round((gps -id $PID).vm/1mb,2)) MB Memory used"
+   $host.ui.RawUI.WindowTitle = "Jason`'s Rocking PowerShell Console: $([Math]::Round((gps -id $PID).WorkingSet/1mb,2)) MB Memory used"
    return ' '
 } 
 $docs = 'VMDrive:\Documents'
 $Down = 'VMDrive:\Downloads'
+Function edit {Powershell_ise $args}
