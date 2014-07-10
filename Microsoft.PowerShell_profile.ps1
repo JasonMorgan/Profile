08deb8617e98990b05d28beb327e06f2df516d45
Jason Morgan <jmorgan@f9vs.com>
2014-07-09 21:28:28 -0700

    Updated Profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,35 +1,21 @@
-<<<<<<< HEAD
-#OkNew-PSDrive -Name VMStore -PSProvider FileSystem -Root '\\VMWare-host\Shared Folders' | out-null
-=======
-#New-PSDrive -Name VMStore -PSProvider FileSystem -Root '\\VMWare-host\Shared Folders' | out-null
->>>>>>> origin/master
+#New-PSDrive -Name Z -PSProvider FileSystem -Root '\\VMWare-host\Shared Folders' | out-null
+Set-Item -Path Env:HOME -Value $HOME
 start-job -ScriptBlock {Update-Help -Force} | Out-Null
+C:\Users\jmorg_000\AppData\Local\GitHub\shell.ps1
 cd c:\git
 function ql { $args }
 function qs ([string]$Separator) { if ($Separator) {$OFS = $Separator} ; "$args" }
 function mf {Param ($path) new-item -itemtype file -path $path -force}
-<<<<<<< HEAD
-#$host.ui.RawUI.WindowTitle = "Jason`'s Rocking PowerShell Console: $([Math]::Round((gps -id $PID).vm/1mb,2)) MB Memory used"
-=======
 $host.ui.RawUI.WindowTitle = "Jason`'s Rocking PowerShell Console: $([Math]::Round((gps -id $PID).WorkingSet/1mb,2)) MB Memory used"
->>>>>>> origin/master
-function prompt { 
-   # our theme
-   $cdelim = [ConsoleColor]::DarkCyan 
-   $chost = [ConsoleColor]::Green 
-   $cloc = [ConsoleColor]::Cyan 
-
-   write-host "$([char]0x0A7) " -n -f $cloc
+Import-Module c:\git\posh-git
+Import-Module PSReadline
+function prompt {
    Write-Host "[$(get-location)]" -NoNewline
-   Write-Host " - Yes Jason?" -ForegroundColor Cyan -NoNewline
+   Write-VcsStatus
+   Write-Host " - Yes Jason?" -ForegroundColor Magenta -NoNewline
    Write-Host " > " -NoNewline
    $host.ui.RawUI.WindowTitle = "Jason`'s Rocking PowerShell Console: $([Math]::Round((gps -id $PID).WorkingSet/1mb,2)) MB Memory used"
    return ' '
-<<<<<<< HEAD
 } 
-=======
-} 
-$docs = "$env:USERPROFILE\Documents"
-$Down = "$env:USERPROFILE\Downloads"
-Function edit {Powershell_ise $args}
->>>>>>> origin/master
+Enable-GitColors
+Start-SshAgent -Quiet
