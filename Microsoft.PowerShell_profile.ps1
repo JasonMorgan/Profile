07ec6f0c706dabd0efd561969f30df239ead5225
jasonmorgan <jmorgan@f9vs.com>
2016-02-05 14:48:12 -0500

    Profile updates
    Still need a general profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,16 @@
 #Set-Item -Path Env:\HOME -Value C:\Users\jmorg_000
 start-job -ScriptBlock {Update-Help -Force} | Out-Null
-C:\Users\jmorg_000\AppData\Local\GitHub\shell.ps1
-[environment]::SetEnvironmentVariable('Home',$HOME)
+#C:\Users\jmorg_000\AppData\Local\GitHub\shell.ps1
+#[environment]::SetEnvironmentVariable('Home',$HOME)
 cd c:\git
 function ql { $args }
-function qs ([string]$Separator) { if ($Separator) {$OFS = $Separator} ; "$args" }
+function qs { "$args" }
 function mf {Param ($path) new-item -itemtype file -path $path -force}
 $Host.PrivateData.ErrorForegroundColor = 'magenta'
 $Host.PrivateData.VerboseForegroundColor = 'cyan'
 $host.ui.RawUI.WindowTitle = "Jason`'s Rocking PowerShell Console: $([Math]::Round((gps -id $PID).WorkingSet/1mb,2)) MB Memory used"
-Import-Module c:\git\posh-git
-Import-Module PSReadline
+#Import-Module c:\git\posh-git
+#Import-Module PSReadline
 function prompt {
    Write-Host "[$(get-location)]" -NoNewline
    Write-VcsStatus
@@ -19,5 +19,5 @@ function prompt {
    $host.ui.RawUI.WindowTitle = "Jason`'s Rocking PowerShell Console: $([Math]::Round((gps -id $PID).WorkingSet/1mb,2)) MB Memory used"
    return ' '
 } 
-Enable-GitColors
-Start-SshAgent -Quiet
+#Enable-GitColors
+#Start-SshAgent -Quiet
